6485a7cf4dadbefbb0616ae70af228a74a29ce10
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2024-03-15 12:01:54 +0100

    Add DockerImage class

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,35 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import fetch from 'node-fetch';
 
+class DockerImage{
+    public registry: string | null = ""
+    public name: string = ""
+    public tag: string = ""
 
+    constructor(registry: string | null, name: string, tag: string){
+        this.registry = registry
+        this.name = name
+        this.tag = tag
+    }
+}
+
+function parse(imageName: string): DockerImage{
+    // TODO parse parts of the image
+    return new DockerImage(null, imageName, "latest")
+}
 
 async function main() {
     // Input
-    const image = core.getInput('image');
+    const imageName = core.getInput('image');
     const registry = core.getInput('registry');
     const username = core.getInput('username');
     const password = core.getInput('password');
-    // Check image
+    // Parse image name and check registries
+    let image = parse(imageName)
+    if (image.registry !== null && registry !== null && image.registry !== registry){
+        throw Error("Registry mismatch, the registry in the image name has to match the registry input")
+    }
+    // Check if image exists
     let exists = false // TODO check if the image exists
     // Return output
     core.setOutput('exists', exists)
